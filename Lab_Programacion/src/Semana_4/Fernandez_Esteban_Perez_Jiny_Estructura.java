0bbdf0214e250fd39af0c9701a21a0a842617bb7
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-05-16 14:17:22 -0600

    Segunda Opcion del  caso del menu
    La funcionalidad del segunda opcion del menu realizada

--- Lab_Programacion/src/Semana_4/Fernandez_Esteban_Perez_Jiny_Estructura.java
+++ Lab_Programacion/src/Semana_4/Fernandez_Esteban_Perez_Jiny_Estructura.java
@@ -19,7 +19,7 @@ public class Fernandez_Esteban_Perez_Jiny_Estructura {
         int opcion = 0;
 
         while (opcion != 4) {
-            
+
             System.out.println("\tMenu");
             System.out.println("1. Cifrado");
             System.out.println("2. Filtrado");
@@ -27,14 +27,61 @@ public class Fernandez_Esteban_Perez_Jiny_Estructura {
             System.out.println("4. Salir");
             System.out.print("Seleccione una opcion del menu: ");
             opcion = scanner.nextInt();
-            
+
             switch (opcion) {
                 case 1:
-                    
+                    System.out.println("Cifrado");
                     break;
                 case 2:
+                    
+                    /*
+                    2 - FILTRAR:
+                    Hacer un programa en Java que permita procesar una frase ingresada por el usuario. El objetivo es extraer y mostrar por \
+                    pantalla todas las palabras cuya longitud sea mayor a un valor N, también proporcionado por el usuario desde el teclado.
+                    El programa debe cumplir con los siguientes pasos:
+                    Solicitar al usuario una frase completa mediante entrada por teclado.
+                    Solicitar un número entero positivo que representará la longitud mínima requerida.
+                    Separar la frase en palabras individuales.
+                    Identificar y mostrar únicamente aquellas palabras que tengan una cantidad de caracteres estrictamente mayor al valor 
+                    ingresado en forma de lista.
+                    Se recomienda ignorar signos de puntuación y considerar como palabras aquellas secuencias de caracteres separadas por espacios.
+                     */
+                    
+                    System.out.println("Filtar");
+
+                    String frase = "",
+                     palabra = "",
+                     palabraslongitud = "";
+                    int longitudMin = 0;
+
+                    System.out.print("Ingrese una frase: ");
+                    frase = scanner.next();
+                    System.out.print("Ingrese una longitud: ");
+                    longitudMin = scanner.nextInt();
+
+                    for (int i = 0; i < frase.length(); i++) {
+                        char letra = ' ';
+                        letra = frase.charAt(i);
+                        if (letra == ' ') {
+                            if (palabra.length() >= longitudMin) {
+                                palabraslongitud += "\n" + palabra;
+                            }
+                            palabra = "";
+                        } else {
+                            palabra += letra;
+                        }
+                    }
+                    if (palabra.length() >= longitudMin) {
+                        palabraslongitud += "\n" + palabra;
+                    }
+
+                    System.out.print("Palabras con longitud de " + longitudMin + ":");
+                    System.out.println(palabraslongitud);
                     break;
+                    
                 case 3:
+                    System.out.println("Codigo Enigma");
+
                     break;
                 case 4:
                     System.out.println("Saliendo del Programa");
@@ -43,7 +90,7 @@ public class Fernandez_Esteban_Perez_Jiny_Estructura {
                 default:
                     System.out.println("Opcion no valida en el menu");
             }
-            
+
         }
 
     }
