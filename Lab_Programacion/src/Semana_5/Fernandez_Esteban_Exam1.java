d2cae5f32fb0a285b5eeecaa3c02eabb8fae7dc4
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-05-23 14:01:57 -0600

    Case 2 del Examen de Lab Programacion I
    Caso 2 del examen de Lab de Programacion con unas mejoras esteticas en los menus y pausas para que el usuario pueda leer

--- Lab_Programacion/src/Semana_5/Fernandez_Esteban_Exam1.java
+++ Lab_Programacion/src/Semana_5/Fernandez_Esteban_Exam1.java
@@ -18,11 +18,15 @@ public class Fernandez_Esteban_Exam1 {
 
         while (opcion != 5) {
 
-            System.out.println("1. Piramide");
-            System.out.println("2. Clave");
-            System.out.println("3. Piedra Papel Tijera");
-            System.out.println("4. Adivinar");
-            System.out.println("5. Salir");
+            System.out.println("");
+            System.out.println("\t\tMenu Principal");
+            System.out.println("----------------------------------------------");
+            System.out.println("| 1. Piramide                                |");
+            System.out.println("| 2. Clave                                   |");
+            System.out.println("| 3. Piedra Papel Tijera                     |");
+            System.out.println("| 4. Adivinar                                |");
+            System.out.println("| 5. Salir                                   |");
+            System.out.println("----------------------------------------------");
             System.out.print("Seleccione una opcion del menu: ");
             opcion = scanner.nextInt();
 
@@ -50,8 +54,87 @@ public class Fernandez_Esteban_Exam1 {
                 case 2:
 
                     /*
-                    
+                    Hacer un programa que permita cifrar y descifrar un mensaje de texto utilizando un cifrado por inversión de letras del alfabeto, 
+                    el texto debe ser ingresado desde el teclado.Cada letra del mensaje será reemplazada por su equivalente opuesto en el alfabeto, 
+                    siguiendo estas reglas:
+                    Las letras minúsculas 'a' a 'z' se invierten:
+                    'a' ↔ 'z', 'b' ↔ 'y', ..., 'm' ↔ 'n'
+                    Las letras mayúsculas 'A' a 'Z' se invierten igual:
+                    'A' ↔ 'Z', 'B' ↔ 'Y', ..., 'M' ↔ 'N'
                      */
+                    String texto = "",
+                     textocifrado = "";
+                    char letra = ' ';
+                    int opcionClave = 0;
+
+                    while (opcionClave != 3) {
+
+                        System.out.println("");
+                        System.out.println("\t\t\tMenu");
+                        System.out.println("------------------------------------------------------");
+                        System.out.println("|     1. Cifrar     2. Descifrar     3. Regresar     |");
+                        System.out.println("------------------------------------------------------");
+
+                        System.out.print("Ingrese una opcion:");
+                        opcionClave = scanner.nextInt();
+
+                        switch (opcionClave) {
+                            case 1:
+
+                                System.out.println("");
+                                System.out.print("Ingrese el texto a cifrar: ");
+                                texto = scanner.next();
+
+                                for (int i = 0; i < texto.length(); i++) {
+                                    letra = texto.charAt(i);
+                                    if (letra >= 'a' && letra <= 'z') {
+                                        textocifrado += (char) ('z' - (letra - 'a'));
+                                    } else if (letra >= 'A' && letra <= 'Z') {
+                                        textocifrado += (char) ('Z' - (letra - 'A'));
+                                    } else {
+                                        textocifrado += letra;
+                                    }
+                                }
+
+                                System.out.println("El texto cifrada es: " + textocifrado + "\n");
+                                texto = "";
+                                textocifrado = "";
+                                break;
+
+                            case 2:
+
+                                System.out.println("");
+                                System.out.print("Ingrese el texto a descifrar: ");
+                                texto = scanner.next();
+
+                                for (int i = 0; i < texto.length(); i++) {
+                                    letra = texto.charAt(i);
+                                    if (letra >= 'a' && letra <= 'z') {
+                                        textocifrado += (char) ('z' - (letra - 'a'));
+                                    } else if (letra >= 'A' && letra <= 'Z') {
+                                        textocifrado += (char) ('Z' - (letra - 'A'));
+                                    } else {
+                                        textocifrado += letra;
+                                    }
+                                }
+
+                                System.out.println("El texto descifrado es: " + textocifrado + "\n");
+                                texto = "";
+                                textocifrado = "";
+                                break;
+
+                            case 3:
+                                System.out.println("");
+                                System.out.println("Regresando");
+                                Thread.sleep(3000);
+                                break;
+                            default:
+                                System.out.println("Opcion no valida en el menu");
+                                break;
+
+                        }
+                    }
+
                     break;
 
                 case 3:
@@ -61,9 +144,18 @@ public class Fernandez_Esteban_Exam1 {
                     break;
 
                 case 5:
+                    System.out.print("Saliendo del Programa");
+                    Thread.sleep(800);
+                    System.out.print(".");
+                    Thread.sleep(800);
+                    System.out.print(".");
+                    Thread.sleep(800);
+                    System.out.println(".");
+                    Thread.sleep(800);
                     break;
 
                 default:
+                    System.out.println("Opcion no valida en el menu ");
                     break;
             }
         }
